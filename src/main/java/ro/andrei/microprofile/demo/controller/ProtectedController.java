ed76d910c6f741d852c8ea84bbfc8d2a4021c3b7
Andrei Avris <andrei.avris@softvision.ro>
2020-05-27 11:19:18 +0300

    Inject token

--- src/main/java/ro/andrei/microprofile/demo/controller/ProtectedController.java
+++ src/main/java/ro/andrei/microprofile/demo/controller/ProtectedController.java
@@ -1,28 +1,34 @@
 package ro.andrei.microprofile.demo.controller;
 
-import org.eclipse.microprofile.jwt.Claim;
-import org.eclipse.microprofile.jwt.ClaimValue;
-
 import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.jwt.Claim;
+import org.eclipse.microprofile.jwt.ClaimValue;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
 /**
  *
  */
 @Path("/protected")
 @RequestScoped
+@Slf4j
 public class ProtectedController {
 
     @Inject
     @Claim("custom-value")
     private ClaimValue<String> custom;
 
+    @Inject
+    private JsonWebToken jwt;
+
     @GET
     @RolesAllowed("protected")
     public String getJWTBasedValue() {
+        log.info(jwt.toString());
         return "Protected Resource; Custom value : " + custom.getValue();
     }
 }
